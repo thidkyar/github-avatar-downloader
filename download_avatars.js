62d6e986041203d0ec2cba736629a71884d726f7
Taranvir Hidkyar <taranvirhidkyar@Taranvirs-MacBook-Pro.local>
2018-05-14 20:39:32 -0400

    'Formatted'

--- download_avatars.js
+++ download_avatars.js
@@ -8,9 +8,9 @@ var repoName = process.argv[3]
 
 function getRepoContributors(repoOwner, repoName, cb) {
   if (process.argv.length !== 4) { //if repoOwner and repoName not specified - end function
-     console.log("Please specify repo owner and repo name")
-     return;
-   };
+    console.log("Please specify repo owner and repo name")
+    return;
+  };
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -18,8 +18,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization': 'token ' + secrets.GITHUB_TOKEN
     }
   };
-var avatarURL = '';
-var avatarPath = '';
+  var avatarURL = '';
+  var avatarPath = '';
   request.get(options, function(err, res, body) {
     cb(err, body);
   });
@@ -28,22 +28,22 @@ var avatarPath = '';
 
 function downloadImageByURL(url, filePath) {
   request.get(url) //get image url
-         .on('error', function (err) {
-           throw err;
-         })
-         .on('response', function(response) {
-           console.log('Response Status Code: ', response.statusCode + response.headers['content-type']);
-         })
-         .pipe(fs.createWriteStream(filePath)); //take in specfified filepath
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log('Response Status Code: ', response.statusCode + response.headers['content-type']);
+    })
+    .pipe(fs.createWriteStream(filePath)); //take in specfified filepath
 }
 
 // console.log('Welcome to the GitHub Avatar Downloader!')
 
 getRepoContributors(repoOwner, repoName, function(err, result) {
-var data = JSON.parse(result) //parse object
-data.forEach(function(x){
-  downloadImageByURL(x.avatar_url, 'avatars/' + x.login) //download file
-})
+  var data = JSON.parse(result) //parse object
+  data.forEach(function(x) {
+    downloadImageByURL(x.avatar_url, 'avatars/' + x.login) //download file
+  })
   // console.log("Errors:", err);
   // console.log("Result:", result);
 });
